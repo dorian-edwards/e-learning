284329d00f62babdfca281a7cba241e0cdcf179c
Dorian Edwards <de.pharmd2018@gmail.com>
2022-08-07 21:36:52 -0400

    add Banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,7 @@
+const Banner = () => (
+  <div className='banner-card font-sans font-extra-bold text-2xl desktop:text-[32px] desktop:leading-[40px] text-white max-w-[350px] p-6 rounded-[10px] sunrise'>
+    Check out our most popular courses!
+  </div>
+)
+
+export default Banner
