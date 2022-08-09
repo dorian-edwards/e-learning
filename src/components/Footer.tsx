b7ce825420d6cbc63a17bbf81fe15351272e3823
Dorian Edwards <de.pharmd2018@gmail.com>
2022-08-09 16:52:49 -0400

    add Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,20 @@
-const Footer = () => <footer>footer</footer>
+import Button from './Button'
 
+const Footer = () => (
+  <footer className='bg-space-cadet'>
+    <div className='container flex justify-between items-center max-w-[1100px] py-9 mx-auto'>
+      <h2 className='font-sans font-extra-bold text-white text-[28px] desktop:text-[36px] leading-[35.28px] desktop:leading-[45.36px]'>
+        skilled
+      </h2>
+      <Button styling='btn-footer' />
+    </div>
+  </footer>
+)
 export default Footer
+
+/**
+ * height 120px
+ * max-width: 1100px
+ * backgorund space-cadet
+ * fonts, same
+ */
